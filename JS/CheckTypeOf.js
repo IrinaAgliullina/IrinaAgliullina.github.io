3146fc0533f81b5d2e16f7f370e3d037d83dadcb
IrinaAgliullina <Agliullina2006@yandex.ru>
2019-03-16 17:03:26 +0300

    Добавлен файл javascript

---
+++ JS/CheckTypeOf.js
@@ -0,0 +1,18 @@
+var a = 7;
+alert("var a = 7, тип данных : " + typeof a); // number
+
+var b = "Строка";
+alert("var b = \"Строка\", тип данных : " + typeof b); // string
+
+var c = {color1: "red", color2: "white"};
+alert("var c = {color1: red, color2: white}, тип данных : " + typeof c); // object
+
+var d = null;
+alert("var d = null, результат alert (d) : " + d); // alert(d) - null
+alert("var d = null, результат alert (typeof d) : " + typeof d); // alert(typeof d) - object
+
+var e = true;
+alert("var e = true, тип данных : " + typeof e); // boolean
+
+var f;
+alert("var f, тип данных : " + typeof f); //undefined
