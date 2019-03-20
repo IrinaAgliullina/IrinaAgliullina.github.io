d33350189e5cde84b510fb208daad09efb97575d
IrinaAgliullina <Agliullina2006@yandex.ru>
2019-03-20 11:38:58 +0300

    Добавлена игра на JS

--- JS/CheckTypeOf.js
+++ JS/CheckTypeOf.js
@@ -16,3 +16,22 @@ alert("var e = true, тип данных : " + typeof e); // boolean
 
 var f;
 alert("var f, тип данных : " + typeof f); //undefined*/
+
+
+function getRandom(min, max) {
+    return Math.floor(Math.random()*11);
+}
+var secretNumber = getRandom(0, 10);
+var i = 0;
+var usersAnswer = prompt("Угадайте число: ", "Целое число от 0 до 10");
+while(usersAnswer != secretNumber) {
+    if(usersAnswer > secretNumber) {
+        usersAnswer = prompt("Загадайте число поменьше:", "Целое число");
+        i = i + 1;
+    } else if (usersAnswer < secretNumber) {
+        usersAnswer = prompt("Загадайте число побольше:", "Целое число");
+        i = i + 1;
+    }
+}
+i = i + 1;
+alert("Вы угадали с " + i + " попыток, это число " + usersAnswer);
