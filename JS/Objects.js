6124abd7f3886ee1b0207eb0d41f1ddeae7d2d22
IrinaAgliullina <Agliullina2006@yandex.ru>
2019-03-26 16:14:37 +0300

    JS - задача на преобразование числа в объект со свойствами: сотни, десятки, единицы.

---
+++ JS/Objects.js
@@ -0,0 +1,37 @@
+// Объекты
+
+/* ДЗ 1:
+   Написать функцию, преобразующую число в объект. Передавая на вход число от 0 до 999, мы
+   должны получить на выходе объект, в котором в соответствующих свойствах описаны
+   единицы, десятки и сотни. Например, для числа 245 мы должны получить следующий объект:
+   {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. Если число превышает 999, необходимо выдать
+   соответствующее сообщение с помощью console.log и вернуть пустой объект.*/
+
+var theNumber = 1000;
+
+var theObject = {};
+
+function expandNumber(theNumber) {
+
+    if(theNumber > 999) {
+        console.log("Число больше 999");
+        return theObject;
+    }
+
+    var x = theNumber / 100;
+    var hundreds = Math.trunc(x);
+
+    var y = (x - hundreds) * 10;
+    var tens = Math.trunc(y);
+
+    var z = (y - tens) * 10;
+    var units = Math.round(z);
+
+    theObject.hundreds = hundreds;
+    theObject.tens = tens;
+    theObject.units = units;
+
+    return theObject;
+}
+
+console.log(expandNumber(theNumber) );
